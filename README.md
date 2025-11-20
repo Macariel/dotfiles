f41d7fabfb66d7084c099a93c522cf3edc4c90b9
Macariel <timo.pawelka@gmail.com>
2025-11-20 12:52:56 +0100

    Update

---
+++ README.md
@@ -0,0 +1,2 @@
+chezmoi init --apply https://github.com/Macariel/dotfiles.git
+(chezmoi cd && git remote set-url origin git@github.com:Macariel/dotfiles.git)
--- run_once_install_basics.sh
+++ run_once_install_basics.sh
@@ -1,6 +1,6 @@
 #!/bin/bash - 
 # Packages
-sudo pacman --needed -Syu git curl gvim python python-pip docker docker-compose jq base-devel networkmanager-openvpn bluez bluez-utils acpica archlinux-keyring unzip man rsync bind
+sudo pacman --needed -Syu git tar curl gvim python python-pip docker docker-compose jq base-devel networkmanager-openvpn bluez bluez-utils acpica archlinux-keyring unzip man rsync bind
 
 # Shell
 sudo pacman --needed -Syu zsh tmux thefuck htop ripgrep tree z fzf tig exa
@@ -27,4 +27,11 @@ fi
 yay --needed -Syu pnpm terraform terragrunt npm nodejs maven gradle jdk-openjdk jdk17-openjdk jdk8-openjdk
 
 # Programs
-yay --needed -S jetbrains-toolbox visual-studio-code-bin firefox google-chrome ferdium-bin guake 1password google-cloud-cli  
+yay --needed -S jetbrains-toolbox visual-studio-code-bin firefox google-chrome ferdium-bin guake 1password 
+
+# GCloud
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-linux-x86_64.tar.gz
+tar -xf google-cloud-cli-linux-x86_64.tar.gz
+sudo mv google-cloud-sdk /opt/google-cloud-sdk
+/opt/google-cloud-sdk/install.sh
+/opt/google-cloud-sdk/bin/gcloud init
