e5d974c75dea563fa194ec16ba17eec9481bcbc4
Macariel <timo.pawelka@gmail.com>
2025-11-24 16:16:41 +0100

    Update

--- run_once_install_basics.sh
+++ run_once_install_basics.sh
@@ -1,9 +1,9 @@
 #!/bin/bash - 
 # Packages
-sudo pacman --needed -Syu git tar curl gvim python python-pip docker docker-compose jq base-devel networkmanager-openvpn bluez bluez-utils acpica archlinux-keyring unzip man rsync bind
+sudo pacman --needed -Syu git tar curl gvim python python-pip docker docker-compose jq base-devel networkmanager-openvpn bluez bluez-utils acpica archlinux-keyring unzip man rsync inetutils bind
 
 # Shell
-sudo pacman --needed -Syu zsh tmux thefuck htop ripgrep tree z fzf tig exa
+sudo pacman --needed -Syu zsh tmux thefuck htop ripgrep tree z fzf tig exa fd
 sudo chsh -s /bin/zsh root
 sudo chsh -s /bin/zsh pawelka
 
@@ -24,10 +24,10 @@ if [[ ! $(command -v yay) ]]; then
 fi
 
 # Work
-yay --needed -Syu pnpm terraform terragrunt npm nodejs maven gradle jdk-openjdk jdk17-openjdk jdk8-openjdk
+yay --needed -Syu pnpm terraform terragrunt npm nodejs maven gradle jdk-openjdk jdk21-openjdk
 
 # Programs
-yay --needed -S jetbrains-toolbox visual-studio-code-bin firefox google-chrome ferdium-bin guake 1password 
+yay --needed -S jetbrains-toolbox visual-studio-code-bin firefox google-chrome ferdium-bin guake 1password 1password-cli
 
 # GCloud
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-linux-x86_64.tar.gz
@@ -35,3 +35,6 @@ tar -xf google-cloud-cli-linux-x86_64.tar.gz
 sudo mv google-cloud-sdk /opt/google-cloud-sdk
 /opt/google-cloud-sdk/install.sh
 /opt/google-cloud-sdk/bin/gcloud init
+
+# Claude Code
+yay -S claude-code
